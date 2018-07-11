d2ed023067bad8b55a80065e2426578b67774d9c
JintGao <jinnit28201@gmail.com>
2018-07-11 19:26:15 +0800

    Update index.php

--- LINE/index.php
+++ LINE/index.php
@@ -289,10 +289,15 @@ function parseInput ($inputStr){
 	}else if(preg_match ("/b/i", $inputStr) !=false){
 		return bDice($inputStr);
 
-	}else if(stristr($inputStr, '判+') !=false 
+	}else if(stristr($inputStr, '判') !=false ||
+			 stristr($inputStr, '*') !=false ||
 			)
 	{
-		return nomalDiceRoller("2d6");
+
+		if(stristr($inputStr, '判+') !=false || stristr($inputStr, '*+') !=false)
+		{
+			return nomalDiceRoller("2d6");
+		}
 	}
 	
 	
