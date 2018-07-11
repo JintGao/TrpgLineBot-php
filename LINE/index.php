4b5b2816d76427256486fbd1eab2f080cf63532b
JintGao <jinnit28201@gmail.com>
2018-07-11 22:43:56 +0800

    Update index.php

--- LINE/index.php
+++ LINE/index.php
@@ -303,7 +303,7 @@ function parseInput ($inputStr){
 			return nomalDiceRoller("2d6".'-'.substr($inputStr,4,strlen($inputStr)) ) ;
 
 		}
-		else if(inputStr=="判")
+		else if($inputStr=="判")
 		{
 			return nomalDiceRoller("2d6");
 		}
@@ -320,7 +320,7 @@ function parseInput ($inputStr){
 		{
 			return nomalDiceRoller("2d6".'-'.substr($inputStr,2,strlen($inputStr)) );
 		}
-		else if(inputStr=="*")
+		else if($inputStr=="*")
 		{
 			return nomalDiceRoller("2d6");
 		}
