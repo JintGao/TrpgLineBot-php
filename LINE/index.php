56f25621a1c8ef599ae78c477b28868d77546c47
JintGao <jinnit28201@gmail.com>
2018-07-11 21:26:12 +0800

    Update index.php

--- LINE/index.php
+++ LINE/index.php
@@ -292,11 +292,11 @@ function parseInput ($inputStr){
 	}else if(stristr($inputStr, '判') !=false)
 	{
 
-		if(mb_substr('判+2',0,2,"utf-8") == '判+')
+		if(mb_substr($inputStr,0,2,"utf-8")=='判+')
 		{
 			return nomalDiceRoller("2d6");
 		}
-		elseif (mb_substr('判+2',0,2,"utf-8") == '判-') 
+		elseif (mb_substr($inputStr,0,2,"utf-8")=='判-') 
 		{
 			return nomalDiceRoller("2d6");
 		}
