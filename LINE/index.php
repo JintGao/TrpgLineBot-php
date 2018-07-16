db8d4b44d51eace89449e8ace46b06a86372c22c
JintGao <jinnit28201@gmail.com>
2018-07-16 21:45:16 +0800

    Update index.php

--- LINE/index.php
+++ LINE/index.php
@@ -337,38 +337,13 @@ function parseInput ($inputStr){
 		}
 		else if($inputStr=="*")
 		{
-			return nomalDiceRoller("2d6");
+			return "你好哦，我是擲骰機器人";
 		}
 	
-	}else if($userName=="Tony高志")
-	{
-			$googledataspi = "https://spreadsheets.google.com/feeds/list/1Hux0vPFA47hZgjPYcoHvMzHP4Q_azPZYDZKnD6H5r78/od6/public/values?alt=json";
-
-			$json = file_get_contents($googledataspi);
-        	$data = json_decode($json, true);     
-        	$store_text=''; 
-			//foreach ($data['feed']['entry'] as $item) 
-			//{    
-				//if($rplyArr[3]  == $item['gsx$姓名']['$t'])
-				//{                
-                //	$store_text = $item['content']['$t'];
-                //}                 
-			//}
-                return $data['feed']['entry'][0]['gsx$content']['$t'];
 	}
 
 	else {
-
-		//if($store_text == '')
-		//{
-		//	$store_text = '呱，沒有這個人或索取的資料喔，建議用 "角色資訊 全部 角色名稱" ，查欄位資料!';
-		//}		
-
-		//return $store_text;
-
-
-
-		return "你好";
+		return null;
 	}
 }
 
