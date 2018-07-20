75939ab3318314eb44c4b854a169d3249557ab43
JintGao <jinnit28201@gmail.com>
2018-07-21 01:12:41 +0800

    Update RulesCustomize.php

--- LINE/RulesCustomize.php
+++ LINE/RulesCustomize.php
@@ -19,27 +19,27 @@ function Rules_Operating($inputStr,$userName) {
 				{
 				     return  buildTextMessage($data['feed']['entry'][$序號]['content']['$t']);
 				}
-				else if($inputStr=='判勇')
+				else if($inputStr=='判勇'||$inputStr=='判勇武' )
 				{
 				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$勇武加值']['$t']);		
 				}
-				else if($inputStr=='判敏')
+				else if($inputStr=='判敏'||$inputStr=='判靈敏')
 				{
 				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$靈敏加值']['$t']);
 				}
-				else if($inputStr=='判魅')
+				else if($inputStr=='判魅'||$inputStr=='判魅力')
 				{
 				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$魅力加值']['$t']);
 				}
-				else if($inputStr=='判意')
+				else if($inputStr=='判意'||$inputStr=='判意志')
 				{
 				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$意志加值']['$t']);
 				}
-				else if($inputStr=='判智')
+				else if($inputStr=='判智'||$inputStr=='判智慧')
 				{
 				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$智慧加值']['$t']);
 				}
-				else if($inputStr=='判精')
+				else if($inputStr=='判精'||$inputStr=='判精神')
 				{
 				     return nomalDiceRoller("2d6".$data['feed']['entry'][$序號]['gsx$精神加值']['$t']);
 				}	    		
