1ac41d3f572f00e492e92a46cd82579f89b69752
JintGao <jinnit28201@gmail.com>
2018-07-20 23:04:26 +0800

    Update RulesCustomize.php

--- LINE/RulesCustomize.php
+++ LINE/RulesCustomize.php
@@ -4,10 +4,46 @@ function Rules_Operating($inputStr,$userName) {
 
 		if($userName='Tony高志')
 		{
-
 			return nomalDiceRoller("2d6+100");	
+		}	
+		else if($userName='羊(Jasmin)')
+		{
+			return nomalDiceRoller("2d6+120");	
 		}
+		else if($userName='Wendy')
+		{
+			return nomalDiceRoller("2d6+120");	
+		}
+		else if($userName='Una Tu')
+		{
+			return nomalDiceRoller("2d6+120");	
+		}		
+		else if($userName='大楷 Kai.Tsai')
+		{
+			return nomalDiceRoller("2d6+120");	
+		}	
+		else if($userName='吳奕歆')
+		{
+			return nomalDiceRoller("2d6+120");	
+		}	
+		else if($userName='Charlie')
+		{
+			return nomalDiceRoller("2d6+120");	
+		}	
+		else if($userName='魷魚')
+		{
+			return nomalDiceRoller("2d6+120");	
+		}	
+		else if($userName='ShiHao')
+		{
+			return nomalDiceRoller("2d6+120");	
+		}	
+		else if($userName='Nash.Lee')
+		{
+			return nomalDiceRoller("2d6+120");	
+		}	
 	
+	 
 		 else if(stristr($inputStr, '勇') !=false)
 		{
 
