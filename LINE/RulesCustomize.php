0c0fc058e8af7388762e3d94bcf3c4000b0f8482
JintGao <jinnit28201@gmail.com>
2018-07-20 21:47:25 +0800

    Update RulesCustomize.php

--- LINE/RulesCustomize.php
+++ LINE/RulesCustomize.php
@@ -2,37 +2,5 @@
 
 function Rules_Operating($inputStr,$userName) { 
 
-		if(stristr($inputStr, '勇') !=false)
-		{
-
-			return nomalDiceRoller("2d6");		
-
-		}
-		else if(stristr($inputStr, '敏') !=false)
-		{
-
-			return nomalDiceRoller("2d6");
-		}
-		else if(stristr($inputStr, '魅') !=false)
-		{
-
-			return nomalDiceRoller("2d6");
-		}
-		else if(stristr($inputStr, '意') !=false)
-		{
-
-			return nomalDiceRoller("2d6");
-		}
-		else if(stristr($inputStr, '智') !=false)
-		{
-			return nomalDiceRoller("2d6");
-
-		}
-		else if(stristr($inputStr, '精') !=false)
-		{
-			return nomalDiceRoller("2d6");
-
-		}
-	
-	return null;
+	return "2d6";	
 }
