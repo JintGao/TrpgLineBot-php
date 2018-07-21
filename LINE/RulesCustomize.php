f41b74cf1ea50fb5d0bffbd5f347eeaac428b548
JintGao <jinnit28201@gmail.com>
2018-07-22 01:40:31 +0800

    Update RulesCustomize.php

--- LINE/RulesCustomize.php
+++ LINE/RulesCustomize.php
@@ -45,20 +45,17 @@ function Rules_Operating($inputStr,$userName) {
 				{
 				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$精神加值']['$t'],$inputStr,$player);
 				}	    		
-				else if($inputStr=='判A'||$inputStr=='判1' || $inputStr=='招式1' 
-					                    || $inputStr==$data['feed']['entry'][$序號]['gsx$A攻擊招式']['$t'] )
+				else if($inputStr=='判A'||$inputStr=='判一')
 				{
-				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$A攻擊數值']['$t'],$inputStr,$player);
+				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$a攻擊數值']['$t'],$inputStr,$player);
 				}				
-				else if($inputStr=='判B'||$inputStr=='判2' || $inputStr=='招式2' 
-					                    || $inputStr==$data['feed']['entry'][$序號]['gsx$B攻擊招式']['$t'] )
+				else if($inputStr=='判B'||$inputStr=='判二')
 				{
-				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$B攻擊數值']['$t'],$inputStr,$player);
+				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$b攻擊數值']['$t'],$inputStr,$player);
 				}
-				else if($inputStr=='判C'||$inputStr=='判3' || $inputStr=='招式3' 
-					                    || $inputStr==$data['feed']['entry'][$序號]['gsx$C攻擊招式']['$t'] )
+				else if($inputStr=='判C'||$inputStr=='判三')
 				{
-				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$C攻擊數值']['$t'],$inputStr,$player);
+				     return nomalDiceRoller_Customize("2d6".$data['feed']['entry'][$序號]['gsx$c攻擊數值']['$t'],$inputStr,$player);
 				}								
 
 	    	}
