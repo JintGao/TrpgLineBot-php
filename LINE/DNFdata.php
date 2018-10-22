073a7b98c3d45386f03dbfa049d9501d45eb6a9e
JintGao <jinnit28201@gmail.com>
2018-10-22 23:48:12 +0800

    Update DNFdata.php

--- LINE/DNFdata.php
+++ LINE/DNFdata.php
@@ -6,9 +6,10 @@ function DNF_Operating($inputStr,$userName) {
 		$googledataspi = "https://spreadsheets.google.com/feeds/list/1OfAW6yFnpSoZov_PeK6MP1u0msRNHXDR8jd7-Hekxyk/1/public/values?alt=json";
 		$json = file_get_contents($googledataspi);
 	    $data = json_decode($json, true); 
-	    $skill='';
 
-		return  buildTextMessage($data['feed']['entry']['0']['gsx$內容']['$t']);	
+	    $序號 =  (int)explode("goto",$inputStr)[1];;
+
+		return  buildTextMessage($data['feed']['entry']['0'][$序號 ]['$t']);	
 	      
 	
 }
