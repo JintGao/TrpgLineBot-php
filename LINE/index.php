ff12ea38e5e178136df2ceded115c64ce35d7093
JintGao <jinnit28201@gmail.com>
2018-10-22 23:16:37 +0800

    Update index.php

--- LINE/index.php
+++ LINE/index.php
@@ -354,7 +354,7 @@ function parseInput ($inputStr){
 	}else if($inputStr=="123")
 	{
 
-          return buildTextMessage("你好");
+          return buildTextMessage("你好123");
 	}
 	else if($inputStr=="456")
 	{
