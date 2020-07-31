efd4c966880c28c0ac353de8df48b89bc63cf4b8
JintGao <jinnit28201@gmail.com>
2020-07-31 19:41:25 +0800

    Update index.php

--- LINE/index.php
+++ LINE/index.php
@@ -367,7 +367,7 @@ function parseInput ($inputStr){
 	}
 	else if($inputStr=="全體擲敏")
 	{
-		return "凡恩" + nomalDiceRoller("2d10+2");
+		return nomalDiceRoller("2d10+2");
 	}
 	else {
 		return null;
